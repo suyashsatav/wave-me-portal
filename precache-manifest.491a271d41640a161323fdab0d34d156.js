ab4c78675263045b945258a4857e3f4bba9900df
Suyash Satav <s0k04qb@walmart.com>
2021-12-18 22:14:13 +0530

    Updates

---
+++ precache-manifest.491a271d41640a161323fdab0d34d156.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e54f1fb3db6be39d02213d26b3087c",
+    "url": "/wave-me-portal/index.html"
+  },
+  {
+    "revision": "dfd6ba2251d20a2207cb",
+    "url": "/wave-me-portal/static/css/main.0e632e19.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/wave-me-portal/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "dfd6ba2251d20a2207cb",
+    "url": "/wave-me-portal/static/js/main.2d8deef3.chunk.js"
+  },
+  {
+    "revision": "1ef8609d64a4c1a0ec88",
+    "url": "/wave-me-portal/static/js/runtime-main.d3da5985.js"
+  }
+]);
