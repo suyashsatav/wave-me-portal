ac245650a62187564d9b11431ea71e3a7767da35
Suyash Satav <s0k04qb@walmart.com>
2021-12-20 22:05:30 +0530

    Updates

--- precache-manifest.97ddb7f7d6fceba141dc764c52042dfe.js
+++ precache-manifest.97ddb7f7d6fceba141dc764c52042dfe.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a4d74cf32e1bde9ee2f38f9faf317fe",
+    "revision": "9c484f5378f7aaa5b2b8d80ba11a5f6e",
     "url": "/wave-me-portal/index.html"
   },
   {
-    "revision": "42ce3932fd7fbfe511e6",
+    "revision": "ad24e142301aba5fb882",
     "url": "/wave-me-portal/static/css/main.eebd02ce.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/wave-me-portal/static/js/2.3dcdfe51.chunk.js"
   },
   {
-    "revision": "42ce3932fd7fbfe511e6",
-    "url": "/wave-me-portal/static/js/main.ebf5bbf6.chunk.js"
+    "revision": "ad24e142301aba5fb882",
+    "url": "/wave-me-portal/static/js/main.f98ac45c.chunk.js"
   },
   {
     "revision": "1ef8609d64a4c1a0ec88",
